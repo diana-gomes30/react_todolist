0046449519438339521ecc24db66d05cc9be4069
Micael Rodrigues <micaelr95@outlook.pt>
2023-01-16 23:31:55 +0000

    cenas

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,109 +1,83 @@
-import React, { useState } from "react";
-import UpdateArea from "./UpdateArea";
-import CreateArea from "./CreateArea";
-import ItemList from "./ItemList";
+import { useState } from 'react';
+import CreateArea from './CreateArea';
+import ItemList from './ItemList';
 
 function App() {
-
   // Tasks (ToDo List) State
   const [taskList, setTaskList] = useState([
-    {"id": 1, "content": "Task 1", "status": false},
-    {"id": 2, "content": "Task 2", "status": true}
+    { id: 1, content: 'Task 1', status: false },
+    { id: 2, content: 'Task 2', status: true },
   ]);
-
-  const [updatingTask, setUpdatingTask] = useState({
-    "id": 0, "content": "", "status": false
-  });
-
-  const [isEditingTask, setIsEditingTask] = useState(false);
+  const [idToEdit, setIdToEdit] = useState(0);
 
   // Add Task
   function addTask(newTask) {
     newTask.id = taskList.length + 1;
-    setTaskList(prevTasks => [...prevTasks, newTask]);
+    setTaskList((prevTasks) => [...prevTasks, newTask]);
   }
 
-  // Delete Task 
+  // Delete Task
   function deleteTask(id) {
-    setTaskList(prevTasks => prevTasks.filter(
-      (taskItem => taskItem.id !== id)
-    ))
+    setTaskList((prevTasks) =>
+      prevTasks.filter((taskItem) => taskItem.id !== id)
+    );
   }
 
   // Mark task done or completed
   function checkTask(id) {
-    setTaskList(prevTasks => prevTasks.map(prevTask => {
-      if (prevTask.id === id) {
-        return { ...prevTask, "status": !prevTask.status };
-      }
-
-      return prevTask;
-    }));
+    setTaskList((prevTasks) =>
+      prevTasks.map((prevTask) =>
+        prevTask.id === id
+          ? { ...prevTask, status: !prevTask.status }
+          : prevTask
+      )
+    );
   }
 
-  // Change task for update
-  function changeTask(task) {
-    setUpdatingTask(task);
-    setIsEditingTask(true);
-  }
-
-  // Cancel Update
-  function cancelUpdate() {
-    setIsEditingTask(false);
-    setUpdatingTask({
-      "id": 0, "content": "", "status": false
-    });
-  }
-
-  // Update Task
   function updateTask(task) {
-    setTaskList(prevTasks => prevTasks.map(prevTask => {
-      if (prevTask.id === task.id) {
-        return { ...prevTask, "content": task.content };
-      }
-
-      return prevTask;
-    }));
-
-    cancelUpdate();
+    setIdToEdit('');
+    setTaskList((prevTasks) =>
+      prevTasks.map((prevTask) =>
+        prevTask.id === task.id
+          ? { ...prevTask, content: task.content }
+          : prevTask
+      )
+    );
   }
+
+  const handleUpdate = (id) => setIdToEdit(id);
 
   return (
     <div className="container app">
-      <br /><br />
+      <br />
+      <br />
       <h1>To Do List App</h1>
-      <br /><br />
+      <br />
+      <br />
 
-      {
-        /* Update Task / Add Task */
-        isEditingTask 
-          ? <UpdateArea 
-              onUpdate={updateTask} 
-              onCancel={cancelUpdate} 
-              updatingTask={updatingTask}
-            /> 
-          : <CreateArea onAdd={addTask} />
-      }
+      <CreateArea
+        onAdd={addTask}
+        handleUpdate={updateTask}
+        text={taskList.filter(({ id }) => id === idToEdit)[0]}
+        setText={setIdToEdit}
+      />
 
-      <br/>
+      <br />
 
       {/* Display Task List */}
       {taskList.length === 0 && <p>No Tasks...</p>}
       {taskList
-        .sort((a,b) => a.id > b.id ? 1 : -1)
-        .map((task, index) => (
-          <ItemList 
+        .sort((a, b) => (a.id > b.id ? 1 : -1))
+        .map((task) => (
+          <ItemList
             key={task.id}
-            index={index}
             task={task}
             onCheck={checkTask}
-            onUpdate={changeTask}
+            onUpdate={() => handleUpdate(task.id)}
             onDelete={deleteTask}
           />
-        ))
-      }
-
-    </div> 
+        ))}
+    </div>
   );
 }
 
--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,43 +1,63 @@
-import React, { useState } from  "react";
+import { useEffect, useState } from 'react';
 
-function CreateArea(props) {
-    const [task, setTask] = useState({
-        "content": "",
-        "status": false
+function CreateArea({ onAdd, handleUpdate, text, setText }) {
+  const [task, setTask] = useState('');
+
+  useEffect(() => {
+    setTask(text?.content);
+  }, [text]);
+
+  function handleChange(event) {
+    setTask(event.target.value);
+  }
+
+  function submitTask() {
+    onAdd(task);
+    setTask({
+      content: '',
+      status: false,
     });
+  }
 
-    function handleChange(event) {
-        setTask({
-            "content": event.target.value, 
-            "status": false
-        });
-    }
+  const handleCancel = () => {
+    setTask('');
+    setText('');
+  };
 
-    function submitTask() {
-        props.onAdd(task);
-        setTask({
-            "content": "", 
-            "status": false
-        });
-    }
-
-    return (
-        <div className="row">
-            <div className="col">
-                <input 
-                    className="form-control form-control-lg"
-                    onChange={handleChange}
-                    name="content" 
-                    value={task && task.content}
-                />
-            </div>
-            <div className="col-auto">
-                <button onClick={submitTask} className="btn btn-lg btn-success">
-                    Add Task
-                </button>
-            </div>
+  return (
+    <div className="row">
+      <div className="col">
+        <input
+          className="form-control form-control-lg"
+          onChange={handleChange}
+          name="content"
+          value={task}
+        />
+      </div>
+      {!text ? (
+        <div className="col-auto">
+          <button onClick={submitTask} className="btn btn-lg btn-success">
+            Add Task
+          </button>
         </div>
-    );
+      ) : (
+        <div className="col-auto">
+          <button
+            onClick={() => {
+              handleUpdate({ ...text, content: task });
+              setTask('');
+            }}
+            className="btn btn-lg btn-success mr-20"
+          >
+            Update
+          </button>
+          <button onClick={handleCancel} className="btn btn-lg btn-warning">
+            Cancel
+          </button>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default CreateArea;
