bf50736326b2704f7c9ed52f76bd43d81e54bf95
PCGAMER-DIANA\Diana <diana.fp.gomes@gmail.com>
2023-01-17 19:42:13 +0000

    Code refactor

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,135 +1,99 @@
-import { useReducer } from "react";
-import UpdateArea from "./UpdateArea";
-import CreateArea from "./CreateArea";
-import ItemList from "./ItemList";
+import { useReducer } from 'react';
+import UpdateArea from './UpdateArea';
+import CreateArea from './CreateArea';
+import ItemList from './ItemList';
+import { reducer } from '../reducer';
 
-const initialTasks = [
-  {
-    "id": 1, 
-    "content": "Task 1", 
-    "status": false
-  },
-  {
-    "id": 2, 
-    "content": "Task 2", 
-    "status": true
-  }
-];
-
-const reducer = (state, action) => {
-  switch (action.type) {
-    case "CHECK":
-      return state.map((task) => (
-        (task.id === action.id) 
-          ? { ...task, status: !task.status }
-          : task  
-      ));
-
-    case "ADD":
-      return [
-        ...state, 
-        {
-          id: state.length > 0 ? state[state.length - 1].id + 1 : 1, 
-          content: action.task.content, 
-          status: action.task.status
-        }
-      ];
-
-    case "UPDATE":
-      return state.map((task) => (
-        (task.id === action.task.id) 
-          ? { ...task, content: action.task.content }
-          : task  
-      ));
-
-    case "DELETE":
-      return state.filter((task => task.id !== action.id));
-
-    case "UPDATING-TASK":
-        return action.task;
-
-    case "CHANGE-TO-UPDATE":
-        return true;
-
-    case "CANCEL-UPDATE":
-        return false;
-    default:
-      return state;
-  }
-}
+const initialState = {
+  taskList: [
+    {
+      id: 1,
+      content: 'Task 1',
+      status: false,
+    },
+    {
+      id: 2,
+      content: 'Task 2',
+      status: true,
+    },
+  ],
+  updatingTask: {},
+  isEditingTask: false,
+};
 
 function App() {
-
   // Tasks (ToDo List) State
-  const [taskList, dispatch] = useReducer(reducer, initialTasks);
-  const [updatingTask, dispatch2] = useReducer(reducer, {});
-  const [isEditingTask, dispatch3] = useReducer(reducer, false);
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   // Add Task
   const addTask = (newTask) => {
-    dispatch({ type: "ADD", task: newTask });
-  }
+    dispatch({ type: 'ADD', task: newTask });
+  };
 
-  // Delete Task 
+  // Delete Task
   const deleteTask = (id) => {
-    dispatch({ type: "DELETE", id: id });
-  }
+    dispatch({ type: 'DELETE', id: id });
+  };
 
   // Mark task done or completed
   const checkTask = (id) => {
-    dispatch({ type: "CHECK", id: id });
-  }
+    dispatch({ type: 'CHECK', id: id });
+  };
 
   // Update Task
   const updateTask = (task) => {
-    dispatch({ type: "UPDATE", task: task });
+    dispatch({ type: 'UPDATE', task: task });
     cancelUpdate();
-  }
+  };
 
   // Change task for update
   function changeTask(task) {
-    dispatch2({ type: "UPDATING-TASK", task: task});
-    dispatch3({ type: "CHANGE-TO-UPDATE" });
+    dispatch({ type: 'UPDATING-TASK', task: task });
+    dispatch({ type: 'CHANGE-TO-UPDATE' });
   }
 
   // Cancel Update
   function cancelUpdate() {
-    dispatch3({ type: "CANCEL-UPDATE" });
-    dispatch2({ 
-      type: "UPDATING-TASK", 
+    dispatch({ type: 'CANCEL-UPDATE' });
+    dispatch({
+      type: 'UPDATING-TASK',
       task: {
-        "id": 0, 
-        "content": "", 
-        "status": false
-      }
+        id: 0,
+        content: '',
+        status: false,
+      },
     });
   }
 
   return (
     <div className="container app">
-      <br /><br />
+      <br />
+      <br />
       <h1>To Do List App</h1>
-      <br /><br />
+      <br />
+      <br />
 
       {
         /* Update Task / Add Task */
-        isEditingTask 
-          ? <UpdateArea 
-              onUpdate={updateTask} 
-              onCancel={cancelUpdate} 
-              updatingTask={updatingTask}
-            /> 
-          : <CreateArea onAdd={addTask} />
+        state.isEditingTask ? (
+          <UpdateArea
+            onUpdate={updateTask}
+            onCancel={cancelUpdate}
+            updatingTask={state.updatingTask}
+          />
+        ) : (
+          <CreateArea onAdd={addTask} />
+        )
       }
 
-      <br/>
+      <br />
 
       {/* Display Task List */}
-      {taskList.length === 0 && <p>No Tasks...</p>}
-      {taskList
-        .sort((a,b) => a.id > b.id ? 1 : -1)
+      {state.taskList.length === 0 && <p>No Tasks...</p>}
+      {state.taskList
+        .sort((a, b) => (a.id > b.id ? 1 : -1))
         .map((task, index) => (
-          <ItemList 
+          <ItemList
             key={task.id}
             index={index}
             task={task}
@@ -137,10 +101,8 @@ function App() {
             onUpdate={changeTask}
             onDelete={deleteTask}
           />
-        ))
-      }
-
-    </div> 
+        ))}
+    </div>
   );
 }
 
--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,40 +1,46 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCircleCheck, faPen, faTrashCan } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faCircleCheck,
+  faPen,
+  faTrashCan,
+} from '@fortawesome/free-solid-svg-icons';
 
 function ItemList(props) {
-    const task = props.task;
+  const task = props.task;
 
-    function handleCheck() {
-        props.onCheck(task.id);
-    }
+  function handleCheck() {
+    props.onCheck(task.id);
+  }
 
-    function handleUpdate() {
-        props.onUpdate(task);
-    }
+  function handleUpdate() {
+    props.onUpdate(task);
+  }
 
-    function handleDelete() {
-        props.onDelete(task.id);
-    }
+  function handleDelete() {
+    props.onDelete(task.id);
+  }
 
-    return (
-        <div key={task.id} className="col taskBg">
-        <div className={task.status ? "done" : ""}>
-          <span className="taskNumber">{task.id}</span>
-          <span className="taskContent">{task.content}</span>
-        </div>
-        <div className="iconsWrap">
-          <span title="Completed / Not Completed">
-            <FontAwesomeIcon onClick={handleCheck} icon={faCircleCheck} />
-          </span>
-          {!task.status && <span title="Edit">
-            <FontAwesomeIcon onClick={handleUpdate} icon={faPen} />
-          </span>}
-          <span title="Delete">
-            <FontAwesomeIcon onClick={handleDelete} icon={faTrashCan} />
-          </span>
-        </div>
+  return (
+    <div key={task.id} className="col taskBg">
+      <div className={task.status ? 'done' : ''}>
+        <span className="taskNumber">{task.id}</span>
+        <span className="taskContent">{task.content}</span>
       </div>
-    );
+      <div className="iconsWrap">
+        <span title="Completed / Not Completed">
+          <FontAwesomeIcon onClick={handleCheck} icon={faCircleCheck} />
+        </span>
+        {!task.status && (
+          <span title="Edit">
+            <FontAwesomeIcon onClick={handleUpdate} icon={faPen} />
+          </span>
+        )}
+        <span title="Delete">
+          <FontAwesomeIcon onClick={handleDelete} icon={faTrashCan} />
+        </span>
+      </div>
+    </div>
+  );
 }
 
 export default ItemList;
